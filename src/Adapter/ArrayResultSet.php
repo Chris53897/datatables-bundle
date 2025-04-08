61bc3fda6e74ac18698e275b224b93b1714be099
Christopher Georg <christopher.georg@sr-travel.de>
2025-04-08 11:30:42 +0200

    feat: upstream changes until 0.10.1

---
+++ src/Adapter/ArrayResultSet.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Symfony DataTables Bundle
+ * (c) Omines Internetbureau B.V. - https://omines.nl/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Omines\DataTablesBundle\Adapter;
+
+/**
+ * SRT (Booking-Engine))
+ *
+ * ArrayResultSet.
+ *
+ * @author Niels Keurentjes <niels.keurentjes@omines.com>
+ *
+ * @phpstan-type Row array<string, mixed>
+ */
+class ArrayResultSet implements ResultSetInterface
+{
+    /** @var Row[] */
+    private array $data;
+    private int $totalRows;
+    private int $totalFilteredRows;
+
+    /**
+     * @param Row[] $data
+     */
+    public function __construct(array $data, ?int $totalRows = null, ?int $totalFilteredRows = null)
+    {
+        $this->data = $data;
+        $this->totalRows = $totalRows ?? count($data);
+        $this->totalFilteredRows = $totalFilteredRows ?? $this->totalRows;
+    }
+
+    public function getTotalRecords(): int
+    {
+        return $this->totalRows;
+    }
+
+    public function getTotalDisplayRecords(): int
+    {
+        return $this->totalFilteredRows;
+    }
+
+    public function getData(): \Iterator
+    {
+        return new \ArrayIterator($this->data);
+    }
+}
